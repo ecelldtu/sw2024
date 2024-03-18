6b0d71da08c41c8e19726ea3bad39cd2e25a7382
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2024-03-19 01:17:00 +0530

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,9 +23,10 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+            North India&apos;s Biggest Startup Event
+            {` `}
             <span className="inline-block relative">
-              Brainwave{" "}
+              Startup Weekend{" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
@@ -35,12 +36,11 @@ const Hero = () => {
               />
             </span>
           </h1>
-          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within Brainwave. Upgrade your productivity
-            with Brainwave, the open AI chat app.
+          <p className="body-1 max-w-3xl mx-auto my-6 text-n-2 lg:my-8">
+            Organized by E-Cell DTU | 5th to 7th April, 2024
           </p>
           <Button href="/pricing" white>
-            Get started
+            Register Now
           </Button>
         </div>
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
