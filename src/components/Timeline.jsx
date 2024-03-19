f0dfd25847360f838f4921eb9b7d0993828ffe91
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2024-03-19 15:20:27 +0530

    timeline done

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,39 @@
+import Heading from "./Heading";
+import Section from "./Section";
+import Tagline from "./Tagline";
+import { roadmap } from "../constants";
+import { Gradient } from "./design/Roadmap";
+
+const Roadmap = () => (
+  <Section className="overflow-hidden" id="roadmap">
+    <div className="container md:pb-10">
+      <Heading tag="Ready to get started" title="Timeline" />
+
+      <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
+        {roadmap.map((item) => {
+          return (
+            <div
+              className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${item.colorful ? "bg-conic-gradient" : "bg-n-6"
+                }`}
+              key={item.id}
+            >
+              <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
+                <div className="relative z-1">
+                  <div className="flex items-center justify-between w-auto mb-8 text-2xl">
+                    <Tagline className="text-base md:text-xl">{item.date}</Tagline>
+                  </div>
+                  <h4 className="h4 mb-4">{item.title}</h4>
+                  <p className="body-2 text-n-4">{item.text}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+
+        <Gradient />
+      </div>
+    </div>
+  </Section>
+);
+
+export default Roadmap;
