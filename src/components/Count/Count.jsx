207171eb1c58260f4bf39a514ec3a2dca4cc08f9
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2025-03-31 19:36:28 +0530

    hmm

--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -1,33 +1,34 @@
 "use client"
 import CountCard from "./CountCard";
-import { useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { motion, useAnimation } from "framer-motion";
+import CountUp from "react-countup";
 export const countCardData = [
   {
-    title: `Domains covered`,
-    count: 20,
-    image: `/domains.png`,
+    title: `Social Reach`,
+    count: 1000000,
+    image: `/images/reach.png`,
   },
   {
-    title: `Judges and Mentors`,
+    title: `Students`,
+    count: 100000,
+    image: `/images/student.png`,
+  },
+  {
+    title: `Startups`,
     count: 50,
-    image: `/speakers.png`,
+    image: `/images/startup.png`,
   },
   {
-    title: `Sponsors`,
-    count: 100,
-    image: `/sponsors.png`,
-  },
-  {
-    title: `Total Attendance`,
-    count: 1000,
-    image: `/attendees.png`,
+    title: `Competitions`,
+    count: 10,
+    image: `/images/funding.png`,
   },
 ];
 
 
-const Count = () => {
+const Count = (props) => {
   const renderCountCards = countCardData.map((countCard, index) => {
     return <CountCard countCard={countCard} key={index} />;
   });
@@ -48,7 +49,7 @@ const Count = () => {
         variants={{
           visible: { opacity: 1, y: 0 },
           hidden: { opacity: 0, y: 60 },
-        }} className="w-full flex justify-center items-center relative h-full mb-24 md:mb-48 md:-mt-32">
+        }} className="w-full flex justify-center items-center relative h-full xl:h-[35rem]">
         <div className="w-full md:w-3/4 lg:w-full grid grid-cols-2 gap-y-16 md:gap-x-4 xl:flex pt-16 md:-mb-12 h-full">
           {renderCountCards}
         </div>
--- src/components/Count/CountCard.jsx
+++ src/components/Count/CountCard.jsx
@@ -1,10 +1,12 @@
+import React from "react";
 import CountUp from "react-countup";
+import Image from "next/image"
 
 const CountCard = (props) => {
   return (
     <div className="flex flex-col justify-center items-center gap-8 px-2 w-full md:w-1/2 lg:w-full xl:w-2/3 py-4">
       <div className="w-auto rounded-full bg-primary-orange flex items-center justify-center">
-        <img width={120} height={120}
+        <Image width={160} height={160}
           src={props.countCard.image}
           alt="sig-pic"
           className="h-full aspect-square p-4"
@@ -18,7 +20,7 @@ const CountCard = (props) => {
         enableScrollSpy={true}
         scrollSpyDelay={10}
       />
-      <h1 className="text-xl text-center -my-4 md:-my-2 font-semibold md:text-2xl text-violet-400 font-code">{props.countCard.title}</h1>
+      <h1 className="text-xl -my-4 md:-my-2 font-semibold md:text-4xl text-violet-400 font-Montserrat">{props.countCard.title}</h1>
     </div>
   );
 };
